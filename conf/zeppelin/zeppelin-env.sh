c49e52da7f986d7a6c1d9961b8e046314abbc74b
Crystalloide <crystalloide@gmail.com>
2026-04-08 01:13:21 +0200

    Create zeppelin-env.sh

---
+++ conf/zeppelin/zeppelin-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
+export HBASE_HOME=/opt/hbase
+export PATH=\$JAVA_HOME/bin:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin:\$HBASE_HOME/bin:\$PATH
+
+# Mémoire Zeppelin server
+export ZEPPELIN_MEM='-Xms512m -Xmx1024m'
+
+# Interpréteur HDFS
+export ZEPPELIN_INTP_MEM='-Xms256m -Xmx512m'
