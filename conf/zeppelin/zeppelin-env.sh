702c65f4fbeba374ab4fce6d171ba31a6f8e3dac
Crystalloide <crystalloide@gmail.com>
2026-04-08 01:25:59 +0200

    Update zeppelin-env.sh

--- conf/zeppelin/zeppelin-env.sh
+++ conf/zeppelin/zeppelin-env.sh
@@ -3,7 +3,7 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export HADOOP_HOME=/opt/hadoop
 export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
 export HBASE_HOME=/opt/hbase
-export PATH=$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$HBASE_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$HBASE_HOME/bin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH
 
 # Mémoire Zeppelin server
 export ZEPPELIN_MEM='-Xms512m -Xmx1024m'
